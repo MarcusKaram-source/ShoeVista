792afd64404dca48803e3e5dc16a55a737af7ea2
MarcusKaram-source <mk859115@gmail.com>
2026-03-14 23:43:55 +0200

    Add About Us nav link, fix backend router mount, add Top Brands to Best Sellers

--- client/src/components/GenInfo.jsx
+++ client/src/components/GenInfo.jsx
@@ -50,6 +50,13 @@ const GenInfo = () => {
 
 export default GenInfo;
 
+export const brandsData = [
+    { src: "/GenInfo/adidas.jpg", name: "Adidas", to: "/search/adidas" },
+      { src: "/GenInfo/nike.png", name: "Nike", to: "/search/nike" },
+        { src: "/GenInfo/skechers.jpg", name: "Skechers", to: "/search/skechers" },
+          { src: "/GenInfo/puma.jpg", name: "Puma", to: "/search/puma" },
+          ];
+]
 export const Brands = () => {
   const navigate = useNavigate();
 
